f6c0a12ad8f1484395eb35dc01a9ab47c6cb5901
Sameer Gogia <sameer.gogia21@gmail.com>
2022-12-28 00:31:39 -0800

    styles updated

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://codebot-hp8g.onrender.com/", {
+  const response = await fetch("https://codebot-hp8g.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
